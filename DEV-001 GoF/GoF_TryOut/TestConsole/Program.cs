48a6849322021cf383a8e448ad61e8b9f87a2850
violettape <my@violet-tape.net>
2014-06-25 17:08:27 +0400

    test

--- DEV-001 GoF/GoF_TryOut/TestConsole/Program.cs
+++ DEV-001 GoF/GoF_TryOut/TestConsole/Program.cs
@@ -1,23 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using GoF_TryOut.AbstractFactory.Flyweight;
 using GoF_TryOut.Builder.Refactored;
-using GoF_TryOut.Commands.Refactored;
-using GoF_TryOut.Composite.Refactored;
-using GoF_TryOut.Composite.Straight;
-using GoF_TryOut.Interpreter.Refactored;
-using GoF_TryOut.Memento.Refactored;
-using GoF_TryOut.Memento.StraightCode;
-using GoF_TryOut.Visitor.Refactored;
 
-namespace TestConsole
-{
-    class Program
-    {
-        static void Main(string[] args) {
+namespace TestConsole {
+    internal class Program {
+        /// <summary>
+        /// Метод для запуска примеров
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main(string[] args) {
             new BuilderExampleRefactored();
 
             Console.ReadLine();
