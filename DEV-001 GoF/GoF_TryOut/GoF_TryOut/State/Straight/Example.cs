b81454235ad05f7e4240fbe16609f9719a4a109b
violettape <my@violet-tape.net>
2014-06-27 16:16:54 +0400

    state

---
+++ DEV-001 GoF/GoF_TryOut/GoF_TryOut/State/Straight/Example.cs
@@ -0,0 +1,18 @@
+﻿namespace GoF_TryOut.State.Straight {
+    public class Example {
+        public Example()
+        {
+            var account = new Account();
+            account.Deposit(1000000);
+            account.Withdraw(500);
+            account.Withdraw(500);
+            account.Withdraw(990000);
+        }
+    }
+
+    public enum CardType {
+        Standard,
+        Gold,
+        Platinum
+    }
+}
