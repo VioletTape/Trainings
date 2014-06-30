7997547d7d6e4fd25b1479f7e0d02bd832076b58
violettape <my@violet-tape.net>
2014-06-30 17:49:32 +0400

    рефакторинг для CoR

--- DEV-001 GoF/GoF_TryOut/GoF_TryOut/ChainOfResposibility/Refactored/Client.cs
+++ DEV-001 GoF/GoF_TryOut/GoF_TryOut/ChainOfResposibility/Refactored/Client.cs
@@ -1,5 +1,33 @@
-﻿namespace GoF_TryOut.ChainOfResposibility.Refactored {
+﻿using System;
+
+namespace GoF_TryOut.ChainOfResposibility.Refactored {
     public class Client {
-         
+        public Client() {
+            var address = new Address();
+            address.DeliveryPriceChanged += () => {
+                if (address.DeliveryPrice > 0) {
+                    Console.WriteLine("Delivery price is " + address.DeliveryPrice);
+                }
+                else {
+                    Console.WriteLine("Can not deliver you postage");
+                }
+            };
+
+            new LocalDeliveryService(
+                new CountryManDelivery(
+                    new LittlePonyExpress(),
+                    new WorldExpress()),
+                new AllUsaDelivery(
+                    new LittlePonyExpress(),
+                    new WorldExpress()),
+                new LittlePonyExpressUsa(
+                    new LittlePonyExpress(),
+                    new WorldExpress())
+                ).CalcDelivery(address);
+
+            new LocalDeliveryService().CalcDelivery(address);
+        }
     }
+
+   
 }
