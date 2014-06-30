6e93fd3525b92ac05eedc06cfaa9b450a6a2c0f4
violettape <my@violet-tape.net>
2014-06-30 16:56:57 +0400

    причесал код для CoR

--- DEV-001 GoF/GoF_TryOut/GoF_TryOut/ChainOfResposibility/Straight/Client.cs
+++ DEV-001 GoF/GoF_TryOut/GoF_TryOut/ChainOfResposibility/Straight/Client.cs
@@ -43,37 +43,5 @@ namespace GoF_TryOut.ChainOfResposibility.Straight {
         }
     }
 
-    internal class WorldExpress : IDeliveryService {
-        public decimal CalcDelivery(Address address) {
-            return 0;
-        }
-    }
-
-    internal class LittlePonyExpress : IDeliveryService {
-        public decimal CalcDelivery(Address address) {
-            return 0;
-        }
-    }
-
-    internal class AllUsaDelivery : IDeliveryService {
-        public decimal CalcDelivery(Address address) {
-            return 0;
-        }
-    }
-
-    internal class CountryManDelivery : IDeliveryService {
-        public decimal CalcDelivery(Address address) {
-            return 0;
-        }
-    }
-
-    public interface IDeliveryService {
-        decimal CalcDelivery(Address address);
-    }
-
-    public class LocalDeliveryService : IDeliveryService {
-        public decimal CalcDelivery(Address address) {
-            return 0;
-        }
-    }
+    
 }
---
+++ DEV-001 GoF/GoF_TryOut/GoF_TryOut/ChainOfResposibility/Straight/IDeliveryService.cs
@@ -0,0 +1,35 @@
+﻿namespace GoF_TryOut.ChainOfResposibility.Straight {
+    public interface IDeliveryService {
+        decimal CalcDelivery(Address address);
+    }
+
+    internal class WorldExpress : IDeliveryService {
+        public decimal CalcDelivery(Address address) {
+            return 0;
+        }
+    }
+
+    internal class LittlePonyExpress : IDeliveryService {
+        public decimal CalcDelivery(Address address) {
+            return 0;
+        }
+    }
+
+    internal class AllUsaDelivery : IDeliveryService {
+        public decimal CalcDelivery(Address address) {
+            return 0;
+        }
+    }
+
+    internal class CountryManDelivery : IDeliveryService {
+        public decimal CalcDelivery(Address address) {
+            return 0;
+        }
+    }
+
+    public class LocalDeliveryService : IDeliveryService {
+        public decimal CalcDelivery(Address address) {
+            return 0;
+        }
+    }
+}
