1d3b1e12e2f5df9d7103552920dcb2e6341daa9c
violettape <my@violet-tape.net>
2014-07-02 16:02:09 +0400

    proxy

--- DEV-001 GoF/GoF_TryOut/GoF_TryOut/Proxy/Refactored/Client.cs
+++ DEV-001 GoF/GoF_TryOut/GoF_TryOut/Proxy/Refactored/Client.cs
@@ -1,5 +1,53 @@
-﻿namespace GoF_TryOut.Proxy.Refactored {
+﻿using System.Drawing;
+using System.Threading.Tasks;
+
+namespace GoF_TryOut.Proxy.Refactored {
     public class Client {
          
+    }
+
+     public abstract class MyImage {
+        private readonly string path;
+        private readonly Bitmap preview;
+        private Bitmap fullImage;
+        private readonly Task<Bitmap> task;
+
+
+        public MyImage(string path) {
+            this.path = path;
+
+            preview = new Bitmap(path);
+
+            task = new Task<Bitmap>(LoadImage);
+        }
+
+
+
+        private Bitmap LoadImage() {
+            return new Bitmap(path);
+        }
+
+        public Bitmap GetPreview() {
+            return preview;
+        }
+
+        public Bitmap GetImage() {
+            if (task.IsCompleted) {
+                if (fullImage == null) {
+                    fullImage = task.Result;
+                    return fullImage;
+                }
+                return fullImage;
+            }
+            return preview;
+        }
+    }
+
+    public class MyImageProxy {
+        
+    }
+
+    public class MyImageReal {
+        
     }
 }
--- DEV-001 GoF/GoF_TryOut/GoF_TryOut/Proxy/Straight/Client.cs
+++ DEV-001 GoF/GoF_TryOut/GoF_TryOut/Proxy/Straight/Client.cs
@@ -1,5 +1,47 @@
-﻿namespace GoF_TryOut.Proxy.Straight {
+﻿using System.Drawing;
+using System.Threading.Tasks;
+
+namespace GoF_TryOut.Proxy.Straight {
     public class Client {
-         
+        public Client() {
+            var myImage = new MyImage("");
+            var bitmap = myImage.GetImage() ?? myImage.GetPreview();
+        }
+    }
+
+    public class MyImage {
+        private readonly string path;
+        private readonly Bitmap preview;
+        private Bitmap fullImage;
+        private Task<Bitmap> task;
+
+
+        public MyImage(string path) {
+            this.path = path;
+
+            preview = new Bitmap(path);
+        }
+
+        private Bitmap LoadImage() {
+            return new Bitmap(path);
+        }
+
+        public Bitmap GetPreview() {
+            return preview;
+        }
+
+        public Bitmap GetImage() {
+            task = new Task<Bitmap>(LoadImage);
+            task.Start();
+
+            if (task.IsCompleted) {
+                if (fullImage == null) {
+                    fullImage = task.Result;
+                    return fullImage;
+                }
+                return fullImage;
+            }
+            return preview;
+        }
     }
 }
